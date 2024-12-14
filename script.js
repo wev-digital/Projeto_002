f99cdcea82877ab841ed1f525d5b747c03bba039
weverton <wevertonmartinsdigital@gmail.com>
2024-12-14 15:09:27 -0300

    Adição de um "main" e um video de introdução juntamente a um botão de oculta o som do video e vice versa

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Programação do botão mobile da web
 function menubar() {
     var menu_mobile = document.querySelector('.nav_lista');
 
@@ -5,5 +6,19 @@ function menubar() {
         menu_mobile.style.display = "none";
     } else {
         menu_mobile.style.display = "block";
+    }
+}
+
+// Programação do botão de alterar o som do vídeo
+function mutevideo() {
+    var home_video = document.querySelector('.video_inicial');
+    var menu_som = document.getElementById('muteIcon');
+
+    if (home_video.muted) {
+        home_video.muted = false;
+        menu_som.setAttribute("name", "volume-high-outline");
+    } else {
+        home_video.muted = true;
+        menu_som.setAttribute("name", "volume-mute-outline");
     }
 }
